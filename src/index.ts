d9a5247f81f23d9784bc93030a47b1ac6c0d88a2
Sebastian Spreizer <spreizer@web.de>
2024-09-26 14:11:29 +0200

    Add nestml

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,52 @@
-import {parser} from "./syntax.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
-import {styleTags, tags as t} from "@lezer/highlight"
+import { completeFromList } from "@codemirror/autocomplete";
+import {
+  LRLanguage,
+  LanguageSupport,
+  delimitedIndent,
+  foldInside,
+  foldNodeProp,
+  indentNodeProp,
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@lezer/highlight";
 
-export const EXAMPLELanguage = LRLanguage.define({
+import { parser } from "./syntax.grammar";
+
+export const nestmlLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({closing: ")", align: false})
+        Application: delimitedIndent({ closing: ")", align: false }),
       }),
       foldNodeProp.add({
-        Application: foldInside
+        Application: foldInside,
       }),
       styleTags({
         Identifier: t.variableName,
         Boolean: t.bool,
         String: t.string,
         LineComment: t.lineComment,
-        "( )": t.paren
-      })
-    ]
+        "( )": t.paren,
+      }),
+    ],
   }),
   languageData: {
-    commentTokens: {line: ";"}
-  }
-})
+    commentTokens: { line: "#" },
+  },
+});
 
-export function EXAMPLE() {
-  return new LanguageSupport(EXAMPLELanguage)
+export const nestmlCompletion = nestmlLanguage.data.of({
+  autocomplete: completeFromList([
+    { label: "model", type: "keyword" },
+    { label: "state", type: "keyword" },
+    { label: "parameters", type: "keyword" },
+    { label: "equations", type: "keyword" },
+    { label: "input", type: "keyword" },
+    { label: "output", type: "keyword" },
+    { label: "update", type: "keyword" },
+    { label: "onCondition", type: "function" },
+  ]),
+});
+
+export function nestml() {
+  return new LanguageSupport(nestmlLanguage, [nestmlCompletion]);
 }
